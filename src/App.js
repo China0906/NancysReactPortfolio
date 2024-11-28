570ef53e7362f27989160d7e33b388684e8b2944
Nancy Ramon <nramon513@gmail.com>
2024-11-28 03:26:14 -0600

    corrected some error in index.html

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Header from "c:/Users/nramo/onedrive/desktop/NancysReactPortfolio/src/components/header.js";
+import Header from "c:/Users/nramo/onedrive/desktop/NancysReactPortfolio/src/components/header";
 import Footer from "c:/Users/nramo/onedrive/desktop/NancysReactPortfolio/src/components/footer";
 import AboutMe from "c:/Users/nramo/onedrive/desktop/NancysReactPortfolio/src/pages/AboutMe";
 import Portfolio from "c:/Users/nramo/onedrive/desktop/NancysReactPortfolio/src/pages/Portfolio";
@@ -14,7 +14,7 @@ function App() {
         <Header />
         <main>
           <Switch>
-            <Route path="/" exact component={AboutMe} />
+            <Route path="/AboutMe" exact component={AboutMe} />
             <Route path="/portfolio" component={Portfolio} />
             <Route path="/contact" component={Contact} />
             <Route path="/resume" component={Resume} />
