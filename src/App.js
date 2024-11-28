8f40b4200453e66cba5c55999c7eeef50f26dc93
Nancy Ramon <nramon513@gmail.com>
2024-11-28 05:19:02 -0600

    deleted header and footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import AboutMe from "./pages/AboutMe";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
